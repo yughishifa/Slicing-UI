b95ff9e0a3667a816ba5cca97132ebca861c6bc1
UserHan9 <raihanindra712@gmail.com>
2025-12-05 19:00:05 +0700

    fixing_bug

--- lib/screen/homepage/homepage_screen.dart
+++ lib/screen/homepage/homepage_screen.dart
@@ -17,7 +17,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
 
-  
   final List<Widget> _pages = [];
 
   @override
@@ -35,9 +34,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],   
+      body: _pages[_currentIndex],
 
-     
       bottomNavigationBar: Container(
         padding: const EdgeInsets.symmetric(vertical: 10),
         decoration: const BoxDecoration(
@@ -53,33 +51,16 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            _navItem(
-              icon: Icons.home,
-              label: "Beranda",
-              index: 0,
-            ),
-            _navItem(
-              icon: Icons.spa,
-              label: "Pantau",
-              index: 1,
-            ),
-            _navItem(
-              icon: Icons.shopping_bag,
-              label: "Market",
-              index: 2,
-            ),
-            _navItem(
-              icon: Icons.person,
-              label: "Profil",
-              index: 3,
-            ),
+            _navItem(icon: Icons.home, label: "Beranda", index: 0),
+            _navItem(icon: Icons.spa, label: "Pantau", index: 1),
+            _navItem(icon: Icons.shopping_bag, label: "Market", index: 2),
+            _navItem(icon: Icons.person, label: "Profil", index: 3),
           ],
         ),
       ),
     );
   }
 
-  
   Widget _navItem({
     required IconData icon,
     required String label,
@@ -115,7 +96,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
- 
   Widget _buildHomePage() {
     return Stack(
       children: [
@@ -141,7 +121,25 @@ class _HomeScreenState extends State<HomeScreen> {
               const SizedBox(height: 24),
               _buildSearchBar(),
               const SizedBox(height: 24),
-              Expanded(child: _buildRecommendationSection()),
+
+              // SCROLLABLE CONTENT
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: _buildRecommendationSection(),
+                    ),
+                    const SizedBox(height: 24),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: _buildStarterKitFlashSale(),
+                    ),
+                    const SizedBox(height: 24),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -149,7 +147,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
- 
   Widget _buildHeader() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
@@ -186,12 +183,15 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 48.0,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              border: Border.all(color: Colors.white, width: 1.5),
+              border: Border.all(color: Colors.black, width: 1.5),
+              color: Colors.white,
             ),
-            child: const Center(
-              child: Text(
-                '👧',
-                style: TextStyle(fontSize: 28),
+            child: ClipOval(
+              child: Image.asset(
+                "lib/core/assets/Memoji Girls 4-12.png",
+                width: 48,
+                height: 48,
+                fit: BoxFit.cover,
               ),
             ),
           ),
@@ -200,7 +200,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  
   Widget _buildProgressCard() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -250,7 +249,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
- 
   Widget _buildSearchBar() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -271,65 +269,59 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  
   Widget _buildRecommendationSection() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              Text(
-                "Rekomendasi Untukmu",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black87,
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Text(
+              "Rekomendasi Untukmu",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                color: Colors.black87,
               ),
-              Text(
-                "Lihat Semua",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: kPrimaryGreen,
-                ),
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 16),
-
-          Expanded(
-            child: GridView.builder(
-              padding: EdgeInsets.zero,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 16,
-                crossAxisSpacing: 16,
-                childAspectRatio: 0.90,
-              ),
-              itemCount: _plants.length,
-              itemBuilder: (context, index) {
-                final p = _plants[index];
-                return _buildPlantCard(
-                  p['image'],
-                  p['name'],
-                  p['level'],
-                  p['duration'],
-                  p['levelColor'],
-                );
-              },
             ),
+            Text(
+              "Lihat Semua",
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: kPrimaryGreen,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 16),
+
+        GridView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+            childAspectRatio: 0.90,
           ),
-        ],
-      ),
+          itemCount: _plants.length,
+          itemBuilder: (context, index) {
+            final p = _plants[index];
+            return _buildPlantCard(
+              p['image'],
+              p['name'],
+              p['level'],
+              p['duration'],
+              p['levelColor'],
+            );
+          },
+        ),
+      ],
     );
   }
 
-  
   Widget _buildPlantCard(
     String image,
     String name,
@@ -392,7 +384,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ],
                     ),
-
                     Row(
                       children: [
                         const Icon(
@@ -413,6 +404,148 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
               ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+  Widget _buildStarterKitFlashSale() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              "Starter Kit Flash Sale 🔥",
+              style: TextStyle(
+                fontSize: 18,
+                color: Color.fromARGB(255, 255, 17, 0),
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+
+            // Timer
+            Row(
+              children: [
+                _flashTimerBox("01"),
+                const SizedBox(width: 4),
+                _flashTimerBox("20"),
+                const SizedBox(width: 4),
+                _flashTimerBox("47"),
+              ],
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 16),
+
+        SizedBox(
+          height: 210,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              _starterKitCard(
+                "lib/core/assets/pakcoy.png",
+                "Paket Pipa NFT",
+                "Rp 125.000",
+                "Rp150.000",
+              ),
+              const SizedBox(width: 16),
+              _starterKitCard(
+                "lib/core/assets/pakcoy.png",
+                "Paket Lengkap",
+                "Rp 55.000",
+                "Rp75.000",
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  static Widget _flashTimerBox(String text) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 255, 0, 0),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
+  Widget _starterKitCard(
+    String img,
+    String title,
+    String price,
+    String oldPrice,
+  ) {
+    return Container(
+      width: 160,
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 6,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(
+              img,
+              height: 100,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          Text(
+            title,
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+            ),
+          ),
+
+          const SizedBox(height: 6),
+
+          Text(
+            price,
+            style: const TextStyle(
+              color: Colors.green,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+          Text(
+            oldPrice,
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 12,
+              decoration: TextDecoration.lineThrough,
             ),
           ),
         ],
